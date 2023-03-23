810411ac79211cca80624eae0cedc24e44544dc0
John Flynn <john.flynn@claytonhomes.com>
2023-03-23 16:21:05 -0400

    Fix swagger page displaying and health enpoints

--- Api/CMH.Template.Api/Controllers/AuthorizationController.cs
+++ Api/CMH.Template.Api/Controllers/AuthorizationController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using CMH.VMF.Security.JwtCommon.Interfaces;
+using Microsoft.Extensions.Options;
 
 namespace CMH.MobileHomeTracker.Api.Controllers
 {
@@ -15,54 +16,38 @@ namespace CMH.MobileHomeTracker.Api.Controllers
     [Authorize]
     public class AuthorizationController : ControllerBase
     {
+        private readonly IOptions<AuthorizationSettings> _authorizationSettings;
         private readonly IJwtGenerator _jwtGenerator;
-        private readonly IJwtValidator _jwtValidator;
 
-        public AuthorizationController(IJwtGenerator jwtGenerator, IJwtValidator jwtValidator)
+        public AuthorizationController(IOptions<AuthorizationSettings> authorizationSettings, IJwtGenerator jwtGenerator)
         {
+            _authorizationSettings = authorizationSettings ?? throw new ArgumentNullException(nameof(authorizationSettings));
             _jwtGenerator = jwtGenerator ?? throw new ArgumentNullException(nameof(jwtGenerator));
-            _jwtValidator = jwtValidator ?? throw new ArgumentNullException(nameof(jwtValidator));
-        }
-
-        /// <summary>Validate a token</summary>
-        /// <param name="jwt">the jwt token to be validated</param>
-        /// <returns>bool: True if valid token, else false</returns>
-        /// <response code="200">The token was validated successfully</response>
-        [HttpPost("validate")]
-        [Produces("application/json")]
-        public async Task<IActionResult> ValidateTokenAsync([FromBody] CMH.VMF.Security.JwtCommon.Models.JWT jwt) 
-        {
-            var ret = await Task.FromResult(_jwtValidator.IsValid(jwt));
-
-            return Ok(ret);
         }
 
         /// <summary>Generate a token</summary>
-        /// <param name="customClaims"> Allows the generation of a new token with custom settings if not null </param>
+        /// <param name="secret">The secret that will be validated and allow a token to be generated</param>
+        /// <remarks>
+        /// ### Validation
+        /// secret
+        /// * Must match the secret configured for this domain and environment
+        /// 
+        /// </remarks>
         /// <returns>A token for this domain</returns>
         /// <response code="200">The Token was generated successfully</response>
-        [HttpPost("generate")]
+        /// <response code="401">The secret does not match the configured value</response>
+        [HttpGet("token/{secret}")]
+        [AllowAnonymous]
         [Produces("application/json")]
         [ProducesResponseType(typeof(CMH.VMF.Security.JwtCommon.Models.JWT), 200)]
-        public async Task<IActionResult> GenerateTokenAsync([FromBody] IEnumerable<System.Security.Claims.Claim> customClaims = null)
+        public async Task<IActionResult> GetTokenAsync(string secret)
         {
-            var ret = customClaims != null ? await Task.FromResult(_jwtGenerator.GenerateToken(customClaims)) : await Task.FromResult(_jwtGenerator.GenerateToken());
+            if(secret != _authorizationSettings.Value.Secret)
+            {
+                return Unauthorized();
+            }
 
-            return Ok(ret);
-        }
-
-        /// <summary>
-        /// This method will strip off all the default claims on the token passed in and add the custom claims to the new token.
-        /// </summary>
-        /// <param name="jwt">Existing Token to be swapped out for an internal token.</param>
-        /// <returns>A token for this domain</returns>
-        /// <response code="200">The Token was swapped successfully</response>
-        [HttpPost("swap")]
-        [Produces("application/json")]
-        [ProducesResponseType(typeof(CMH.VMF.Security.JwtCommon.Models.JWT), 200)]
-        public async Task<IActionResult> SwapTokenAsync([FromBody] CMH.VMF.Security.JwtCommon.Models.JWT jwt)
-        {
-            var ret = await Task.FromResult(_jwtGenerator.SwapToken(jwt));
+            var ret = await Task.FromResult(_jwtGenerator.JWT);
 
             return Ok(ret);
         }
